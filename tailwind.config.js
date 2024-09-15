9c8e1ea20cd9bd86d4701855244e519853d630a9
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-09-15 19:21:16 +0530

    Home Page is compleated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#FD8814",
+        primarylight: "#FFD5BC",
+        secondary: "#3E2E55",
+        light: "#F8F7FF",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
